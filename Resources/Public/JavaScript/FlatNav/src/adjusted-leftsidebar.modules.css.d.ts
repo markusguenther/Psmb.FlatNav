d6f0213fedce2dbda02db334de72e3ecefaa7fef
Markus Günther <info@unikka.de>
2024-10-04 20:41:32 +0200

    TASK: Adjust sizing of node items
    As we are able to resize the left sidebar nowadays we need to adjust the size and ellipsis handling.

---
+++ Resources/Public/JavaScript/FlatNav/src/adjusted-leftsidebar.modules.css.d.ts
@@ -0,0 +1,8 @@
+export declare const leftSideBarAdjusted: string;
+export declare const leftSideBarTop: string;
+
+declare const ClassNames: {
+  "leftSideBarAdjusted": string;
+  "leftSideBarTop": string;
+};
+export default ClassNames;
